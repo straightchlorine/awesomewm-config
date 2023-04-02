9686876bce4ec73f51c559a092de234097dc5f2a
Piotr Krzysztof <piotrlis555@gmail.com>
2023-04-02 19:18:37 +0200

    apps: more or less structured apps.lua file

--- configuration/bindings/apps.lua
+++ configuration/bindings/apps.lua
@@ -1,3 +1,22 @@
 return {
-  
+  -- default applications
+  default = {
+    terminal = 'alacritty',
+    browser = 'firefox',
+    text_editor = 'nvim',
+    file_manager = 'thunar',
+    launcher = 'steam-native'
+    -- add rofi, and other things
+  },
+
+  -- applications to run on startup
+  autostart = {
+    -- get here compositor (maybe give hyprland a shot)
+  },
+
+  -- utility scripts
+  utilities = {
+    -- screenshots (full and area)
+  }
+
 }
